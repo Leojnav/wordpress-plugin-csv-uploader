38af9a5fce0704cd87650ebb2c63297db6550cf8
Leojnav <94356230+Leojnav@users.noreply.github.com>
2025-07-30 13:59:11 +0200

    Delete test-template directory

--- test-template/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-    function enqueue_theme_styles() {
-        wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/style.css', array(), '1.0', 'all');
-    }
-    add_action('wp_enqueue_scripts', 'enqueue_theme_styles');
-?>
